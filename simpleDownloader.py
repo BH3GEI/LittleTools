c1f2b06fbfe9120ebb347cd9a0f5978d3bd1b137
BH3GEI <58540850+BH3GEI@users.noreply.github.com>
2023-10-31 14:04:20 +0800

    Update simpleDownloader.py

--- simpleDownloader.py
+++ simpleDownloader.py
@@ -2,94 +2,75 @@ import os
 import requests
 import threading
 from urllib.parse import urlparse
-from tkinter import filedialog, Text, Button, Toplevel, Tk, ttk, Entry, StringVar, Label, messagebox
+from tkinter import filedialog, Text, Button, Toplevel, Tk, ttk
 import re
 import mimetypes
 import errno
 
-class App:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("下载器")
+from tqdm import tqdm
 
-        self.download_path = StringVar()
 
-        self.path_label = Label(master, text="下载路径：")
-        self.path_label.grid(row=0, column=0)
+class InputDialog(Toplevel):
+    def __init__(self, parent):
+        Toplevel.__init__(self, parent)
+        self.title("请输入包含链接的字符串~喵~")
+        self.text = Text(self, width=80, height=20)
+        self.text.pack()
+        self.button = Button(self, text="确定~喵~", command=self.ok)
+        self.button.pack()
 
-        self.path_entry = Entry(master, textvariable=self.download_path, width=50)
-        self.path_entry.grid(row=0, column=1)
+    def ok(self):
+        self.input_string = self.text.get(1.0, "end")
+        self.destroy()
 
-        self.path_button = Button(master, text="选择路径", command=self.choose_path)
-        self.path_button.grid(row=0, column=2)
-
-        self.input_label = Label(master, text="输入链接：")
-        self.input_label.grid(row=1, column=0)
-
-        self.input_text = Text(master, width=60, height=10)
-        self.input_text.grid(row=1, column=1, columnspan=2)
-
-        self.start_button = Button(master, text="开始", command=self.start_download)
-        self.start_button.grid(row=2, column=0)
-
-        # self.pause_button = Button(master, text="暂停", command=self.pause_download)
-        # self.pause_button.grid(row=2, column=1)
-        #
-        # self.stop_button = Button(master, text="停止", command=self.stop_download)
-        # self.stop_button.grid(row=2, column=2)
-
-        self.progress_label = Label(master, text="下载进度：")
-        self.progress_label.grid(row=3, column=0)
-
-        self.progress = ttk.Progressbar(master, length=200)
-        self.progress.grid(row=3, column=1, columnspan=2)
-
-    def choose_path(self):
-        self.download_path.set(filedialog.askdirectory())
-
-    def start_download(self):
-        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', self.input_text.get(1.0, "end"))
-        for url in urls:
-            threading.Thread(target=self.download_file, args=(url, self.download_path.get())).start()
-
-    def pause_download(self):
-        # 未实现
-        pass
-
-    def stop_download(self):
-        # 未实现
-        pass
-
-    def download_file(self, url, download_dir):
-        path = urlparse(url).path
-        filename = os.path.basename(path)
-        if filename == "":
-            response = requests.head(url)
-            if 'Content-Type' in response.headers:
-                content_type = response.headers['Content-Type']
-                ext = mimetypes.guess_extension(content_type)
-                filename = "downloaded_file" + (ext if ext else "")
-        file_path = os.path.join(download_dir, filename)
-        if os.path.exists(file_path):
-            if not messagebox.askyesno("文件已存在~", f"文件 '{filename}' 已存在，链接：'{url}'。是否替换？"):
-                return
-        if not os.path.exists(os.path.dirname(file_path)):
-            try:
-                os.makedirs(os.path.dirname(file_path))
-            except OSError as exc:
-                if exc.errno != errno.EEXIST:
-                    print(f"Failed to create directory due to {str(exc)}~")
-                    return
+def download_file(url, file_path):
+    try:
         with requests.get(url, stream=True) as r:
             r.raise_for_status()
-            total_size = int(r.headers.get('content_length', 0))
-            block_size = 1024
+            total_size = int(r.headers.get('content-length', 0))
+            block_size = 1024 #1 Kibibyte
+            t=tqdm(total=total_size, unit='iB', unit_scale=True)
             with open(file_path, 'wb') as f:
                 for data in r.iter_content(block_size):
+                    t.update(len(data))
                     f.write(data)
-                    self.progress['value'] += len(data)
-                self.progress['maximum'] = total_size
+            t.close()
+            if total_size != 0 and t.n != total_size:
+                print("ERROR, something went wrong~喵~")
+    except Exception as e:
+        print(f"Failed to download {url} to {file_path} due to {str(e)}~喵~")
 
 root = Tk()
-app = App(root)
-root.mainloop()
+root.withdraw()
+
+dialog = InputDialog(root)
+root.wait_window(dialog)
+input_string = dialog.input_string
+
+urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', input_string)
+
+download_dir = filedialog.askdirectory()
+
+for url in urls:
+    path = urlparse(url).path
+    filename = os.path.basename(path)
+
+
+    if filename == "":
+        response = requests.head(url)
+        if 'Content-Type' in response.headers:
+            content_type = response.headers['Content-Type']
+            ext = mimetypes.guess_extension(content_type)
+            filename = "downloaded_file" + (ext if ext else "")
+
+    file_path = os.path.join(download_dir, filename)
+
+    if not os.path.exists(os.path.dirname(file_path)):
+        try:
+            os.makedirs(os.path.dirname(file_path))
+        except OSError as exc: # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                print(f"Failed to create directory due to {str(exc)}~喵~")
+                continue
+                
+    threading.Thread(target=download_file, args=(url, file_path)).start()
