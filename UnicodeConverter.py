8d61cf0c91ecebb9b3396cc6c22c504a82466716
BH3GEI <58540850+BH3GEI@users.noreply.github.com>
2023-12-15 11:29:36 +0800

    Updated UnicodeConverter.py

---
+++ UnicodeConverter.py
@@ -0,0 +1,15 @@
+decimal_values = [32472, 22270, 26085, 24535, 65292, 21452, 20987, 21487, 20197, 26597, 30475, 35814, 32454, 21382, 21490, 35760, 24405]
+
+text = ''.join(chr(decimal) for decimal in decimal_values)
+
+print(text)
+
+chinese_text = '你可以看，和操作能以观看详细历史记录'
+
+decimal_values = [ord(char) for char in chinese_text]
+
+print(decimal_values)
+
+encoded_text = ''.join(f"#{ord(char)}" for char in chinese_text)
+
+print(encoded_text)
